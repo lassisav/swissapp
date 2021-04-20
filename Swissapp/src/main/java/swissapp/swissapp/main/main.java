e35b709a9587092d8427594568c1c0f584ac485f
lassisav <lassi.savolainen@helsinki.fi>
2021-04-20 16:35:03 +0300

    Added automatic creation of necessary files and directories to main method, before starting GUI.

--- Swissapp/src/main/java/swissapp/swissapp/main/main.java
+++ Swissapp/src/main/java/swissapp/swissapp/main/main.java
@@ -5,15 +5,30 @@
  */
 package swissapp.swissapp.main;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import swissapp.swissapp.UI.GraphicUI;
 
 /**
  *
  * @author lassisav
- * NOTE!! Currently to run this program, you need to create directory swissapp/Swissapp/tourneyfiles/db. TODO: Automate directory creation
  */
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File temp = new File("");
+        String pathName = temp.getAbsolutePath();
+        pathName = pathName.concat("/tourneyfiles");
+        Path path = Paths.get(pathName);
+        if (!Files.exists(path)) {
+            File f1 = new File(pathName.concat("/db"));
+            f1.mkdirs();
+        }
+        File f2 = new File(pathName.concat("/db/idList.txt"));
+        f2.createNewFile();
+        temp.delete();
         GraphicUI.main(args);
     }
 }
